bae3bbc3b22efac0aeddf32ad18f5010354e095c
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-06-01 11:53:42 +0200

    Course component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursComponent } from './cours/cours.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CoursComponent
   ],
   imports: [
     BrowserModule
