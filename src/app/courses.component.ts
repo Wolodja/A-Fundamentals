90be7a11b9fe053573ac2c7b9a96db0dcd868c4a
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-06-03 12:08:56 +0200

    Event Filtering (ENTER event)

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <div (click)="onDivClicked()">
-            <button (click)="onSave($event)" >Save</button>
-        </div>
-    `   
+        <input (keyup.enter)="onKeyUp()" />
+    `
 })
 export class CoursesComponent {
 
-    onSave($event){
-        $event.stopPropagation();
-        console.log("Button was clicked", $event);
-    }
+    onKeyUp($event) {
 
-    onDivClicked(){
-        console.log("Div was clicked");
+        console.log("ENTER was pressed");
+
     }
 
 }
