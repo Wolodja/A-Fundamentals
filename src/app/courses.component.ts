3103555e96732f260b56af14d186f7a9205794cb
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-06-03 11:39:35 +0200

    attribute binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,25 +4,17 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-    <h2>{{ title }}</h2>
-    <ul>
-        <li *ngFor="let course of courses">
-            {{ course }}
-        </li>
-    </ul>
-    <img src = "{{ imageUrl }}" />
     <img [src] = "imageUrl" />
+    <table>
+        <tr>
+            <td [attr.colspan] = "colSpan"></td>
+        </tr>
+    </table>
     ` 
 })
 export class CoursesComponent {
-    title = "List of courses";
 
-    courses;
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
-
-    imageUrl = "http://lorempixel.com/400/200";
+    imageUrl = "";
+    colSpan = 2;
 
 }
