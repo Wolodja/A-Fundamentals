6142f4af395c7a93ea519eaab6bdb77599154bee
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-06-23 13:44:26 +0200

    ngForm

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent {
 
-  log(x){ console.log(x);}
+  log(x) { console.log(x); }
+
+  submit(f) { console.log(f); }
 
 }
