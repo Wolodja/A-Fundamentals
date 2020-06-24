284cb72010e9cb319124612051e81b7b7680dc92
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-06-24 18:00:50 +0200

    Multiple validators in reactive form

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -9,11 +9,11 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class SignupFormComponent {
 
   form = new FormGroup({
-    username: new FormControl('', Validators.required),
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
     password: new FormControl()
   });
 
-  get username(){
+  get username() {
     return this.form.get('username');
   }
 }
