475cfb468373939aea0566d80082de5666bdca6a
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-08-12 11:08:42 +0200

    Http POST for creating objects

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,14 +9,25 @@ import { HttpClient } from '@angular/common/http';
 export class PostsComponent {
 
   posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: HttpClient) {
-
-    http.get<any[]>('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: HttpClient) {
+    http.get<any[]>(this.url)
       .subscribe(response => {
         this.posts = response;
         console.log(this.posts);
       });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+
+    this.http.post(this.url, post)
+      .subscribe(response => {
+        this.posts.splice(0, 0, response);
+      });
+
   }
 
 
