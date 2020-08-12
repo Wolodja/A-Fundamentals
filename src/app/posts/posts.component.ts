ac9be867d03bf8ebd22332e74893f8df826333d1
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-08-12 12:30:04 +0200

    Http PATCH for updating objects

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -27,7 +27,15 @@ export class PostsComponent {
       .subscribe(response => {
         this.posts.splice(0, 0, response);
       });
+  }
 
+  updatePost(post) {
+
+    console.log(post.id);
+    this.http.patch(this.url + '/' + post.id, { title: 'New Title' })
+      .subscribe(response => {
+        post.title = response['title'];
+      });
   }
 
 
