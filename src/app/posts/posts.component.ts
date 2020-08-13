a74d5651c6613745b5d26c43e28192c560ecfe66
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-08-13 13:33:59 +0200

    Handling unexpected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -17,7 +17,9 @@ export class PostsComponent implements OnInit {
     this.service.getPosts()
       .subscribe(response => {
         this.posts = response;
-        console.log(this.posts);
+      }, error => {
+        alert('An unexpected error occured!');
+        console.log(error);
       });
   }
 
@@ -28,6 +30,9 @@ export class PostsComponent implements OnInit {
     this.service.createPost(post)
       .subscribe(response => {
         this.posts.splice(0, 0, response);
+      }, error => {
+        alert('An unexpected error occured!');
+        console.log(error);
       });
   }
 
@@ -35,6 +40,9 @@ export class PostsComponent implements OnInit {
     this.service.updatePost(post)
       .subscribe(response => {
         post.title = response['title'];
+      }, error => {
+        alert('An unexpected error occured!');
+        console.log(error);
       });
   }
 
@@ -43,6 +51,9 @@ export class PostsComponent implements OnInit {
       .subscribe(response => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
+      }, error => {
+        alert('An unexpected error occured!');
+        console.log(error);
       })
   }
 }
