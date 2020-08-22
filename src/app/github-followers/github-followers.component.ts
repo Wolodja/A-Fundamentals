2349df177491da07ea712fcae75fad57f714f15c
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-08-22 19:11:56 +0200

    query parameters and subscribing to multiple obs

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,7 @@
 import { GithubFollowersService } from './../github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'github-followers',
@@ -10,11 +12,18 @@ export class GithubFollowersComponent implements OnInit {
 
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(private route: ActivatedRoute, private service: GithubFollowersService) { }
 
   ngOnInit(): void {
-    this.service.getAll()
-    .subscribe(followers => this.followers = followers)
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+      this.service.getAll()
+        .subscribe(followers => this.followers = followers)
+    })
   }
 
 }
