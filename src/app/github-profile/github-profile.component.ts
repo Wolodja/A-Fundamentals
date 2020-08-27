e7eea90dc6a786f11e065b766cafea43c6ae9478
Volodymyr Vaskovych <wolodjawaskovych@i.ua>
2020-08-27 11:14:27 +0200

    Programmatic navigation

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
-export class GithubProfileComponent implements OnInit {
+export class GithubProfileComponent {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: Router) { }
 
-  ngOnInit() {
-    this.route.paramMap
-      .subscribe(params => {
-        let id = +params.get('id');
-        console.log(id);
-      });
+  submit(){
+    this.route.navigate(['/followers'], {
+      queryParams : {
+        page: 1,
+        order: 'newest'
+      }
+    })
   }
+
+
 
 }
